313ad383553e3fc75c77c336e181981a6f0c521d
ljwoon1211 <ljwoon1211@gmail.com>
2025-04-28 15:55:35 +0900

    refactor: shared/ui dialog 분리

---
+++ src/shared/ui/dialog/DialogTitle.tsx
@@ -0,0 +1,14 @@
+import { forwardRef } from "react"
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+
+export const DialogTitle = forwardRef<
+  React.ComponentRef<typeof DialogPrimitive.Title>,
+  React.ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={`text-lg font-semibold leading-none tracking-tight ${className}`}
+    {...props}
+  />
+))
+DialogTitle.displayName = DialogPrimitive.Title.displayName
